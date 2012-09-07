996ae65a4523ac7e44fbe8cdff305b92d9671b34
sherpc <aleksandrsher@gmail.com>
2012-09-08 01:43:23 +0400

    Added get_json_from_google

--- server/main.rb
+++ server/main.rb
@@ -1,11 +1,20 @@
 require "google_drive"
 
-# Logs in.
-# You can also use OAuth. See document of
-# GoogleDrive.login_with_oauth for details.
-session = GoogleDrive.login("aleksandrsher@gmail.com", "G0Dsp#ll")
+def get_json_from_google
+  session = GoogleDrive.login("aleksandrsher@gmail.com", "G0Dsp#ll")
+  key = "0AmvGiQY4WDYHdFhxdnI1R0lYY19WREFBX21KdHRlZ0E"
+  ws = session.spreadsheet_by_key(key).worksheets[2]
+  data = []
+  ws.rows.drop(1).each do |row|
+    student = {}
+    student["name"] = row[0]+" "+row[1]
+    student["seminar"] = row[2]
+    student["lab1"] = row[3]
+    student["lab2"] = row[4]
+    student["exam"] = row[5]
+    data << student
+  end
+  data
+end
 
-key = "0AmvGiQY4WDYHdFhxdnI1R0lYY19WREFBX21KdHRlZ0E"
-ws = session.spreadsheet_by_key(key).worksheets[2]
-
-p ws.rows[1]
+get_json_from_google
